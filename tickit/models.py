21a327f445986bfeb1abbf68b60156790625f99b
chrischen <chrischen96@outlook.com>
2023-07-18 16:03:49 -0700

    create venue and event models

--- tickit/models.py
+++ tickit/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    website = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='events')
+
+    def __str__(self) -> str:
+        return self.name
