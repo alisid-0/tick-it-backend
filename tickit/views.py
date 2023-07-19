6ba231525546dbe0258aa6c65b8c72282271a182
Shay <fwasil83@gmail.com>
2023-07-18 23:15:52 -0400

    rest framework

--- tickit/views.py
+++ tickit/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Venue, Event
+from .serializers import VenueSerializer, EventSerializer
 
-# Create your views here.
+class VenueList(generics.ListCreateAPIView):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+class VenueDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
