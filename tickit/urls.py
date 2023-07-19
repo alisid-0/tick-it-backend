d3a95ac3850b6972cb09863f0809f2a0e679c345
chrischen <chrischen96@outlook.com>
2023-07-19 08:21:04 -0700

    add / path

--- tickit/urls.py
+++ tickit/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.VenueList.as_view(), name='venue-list'),
     path('venues/', views.VenueList.as_view(), name='venue-list'),
     path('venues/<int:pk>', views.VenueDetail.as_view(), name='venue-detail'),
     path('events/', views.EventList.as_view(), name='event-list'),
